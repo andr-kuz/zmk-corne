51b9fc70e2cd6f72410b646f4f499072e7ec13a9
Andrei Kuznetsov <awakum192@gmail.com>
2025-10-24 20:16:00 +0300

    feat: persistent special symbols accross layouts

---
+++ config/extra_keys.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+#define ENGLISH_LAYOUT LC(LGUI)
+#define LAST_LAYOUT LC(LS(SPACE))
+#define NEXT_LAYOUT LC(SPACE)
