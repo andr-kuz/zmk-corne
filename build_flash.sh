4b9fce1db4922a59fda47851cfa32c38b8dccb24
awakum <awakum192@gmail.com>
2025-12-26 16:11:59 +0300

    test

--- build_flash.sh
+++ build_flash.sh
@@ -12,9 +12,8 @@ build_and_flash() {
     if [ -n "$archive_path" ]; then
         echo "Extracting $side side from $archive_path..."
         mkdir -p build/zephyr
-        unzip -p "$archive_path" "corne_$side-nice_nano_v2-zmk.uf2" > build/zephyr/corne_$side.uf2
+        unzip -p "$archive_path" "corne_$side-nice_nano_v2-zmk.uf2" > "build/zephyr/corne_$side.uf2"
     else
-
         echo "Building $side side..."
         west build -p -b nice_nano_v2 zmk/app -- -DZEPHYR_BASE=$(pwd)/zephyr -DZEPHYR_SDK_INSTALL_DIR=~/zephyr-sdk-0.16.5 -DSHIELD=corne_$side -DZMK_CONFIG=$(pwd)/config
         mv build/zephyr/zmk.uf2 build/zephyr/corne_$side.uf2
@@ -26,10 +25,11 @@ build_and_flash() {
     done
     
     udisksctl mount -b "$FULL_PATH"
-    cp build/zephyr/corne_$side.uf2 /run/media/valtrois/NICENANO/
+    cp "build/zephyr/corne_$side.uf2" "/run/media/valtrois/NICENANO/"
     udisksctl unmount -b "$FULL_PATH"
-    udisksctl power-off -b "$FULL_PATH"
+    # udisksctl power-off -b "$FULL_PATH"
     echo "Done with $side side"
+    sleep 5
 }
 
 build_and_flash left $archive_path
